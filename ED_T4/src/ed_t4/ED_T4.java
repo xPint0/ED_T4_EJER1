9778ac0f5fd92a39990257c7643d8f2a6766a2a4
pinto <pinvalda@lab03-08.gregoriofer.local>
2023-01-10 13:18:09 +0100

    Sexta confirmacion

---
+++ ED_T4/src/ed_t4/ED_T4.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ed_t4;
+
+/**
+ *
+ * @author pinvalda
+ */
+public class ED_T4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public class Votacion {
+        private String nombre;
+        private Integer votos;
+        
+        public Votacion(){
+            
+        }
+
+        public Votacion(String nombre, Integer votos) {
+            super();
+            this.nombre = nombre;
+            this.votos = votos;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public Integer getVotos() {
+            return votos;
+        }
+
+        public void setVotos(Integer votos) {
+            this.votos = votos;
+        }
+        
+    }
+    
+}
